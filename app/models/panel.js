7435c07faf39302a16eb189af2d1f1531dbc7567
jj.jpg <jayjayjpg@gmail.com>
2021-05-24 21:11:03 +0200

    upd(index): layouting work

--- app/models/panel.js
+++ app/models/panel.js
@@ -2,4 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class PanelModel extends Model {
   @attr url;
+  @attr layoutRowClass;
+  @attr layoutColumnClass;
+  @attr otherClasses;
 }
--- mirage/factories/panel.js
+++ mirage/factories/panel.js
@@ -4,4 +4,16 @@ export default Factory.extend({
   url(i) {
     return `https://picsum.photos/id/${i + 10}/300/300`; // Movie 1, Movie 2, etc.
   },
+
+  layoutColumnClass() {
+    return `col-end-${Math.floor(Math.random() * 4) + 1}`;
+  },
+
+  layoutRowClass(i) {
+    return `row-end-${Math.floor(Math.random() * 4) + 1}`;
+  },
+
+  otherClasses(i) {
+    return `abc-${i}`;
+  },
 });
