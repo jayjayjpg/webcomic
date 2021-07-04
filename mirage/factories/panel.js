3753bbdd03a7fdef8f0fee60333297b88390e007
jj.jpg <jayjayjpg@gmail.com>
2021-07-04 23:01:32 +0200

    feat(home): create container element

--- mirage/factories/panel.js
+++ mirage/factories/panel.js
@@ -5,12 +5,20 @@ export default Factory.extend({
     return `https://picsum.photos/id/${i + 10}/300/300`; // Movie 1, Movie 2, etc.
   },
 
-  layoutColumnClass() {
+  /* layoutColumnClass() {
     return `col-end-${Math.floor(Math.random() * 4) + 1}`;
   },
 
   layoutRowClass(i) {
     return `row-end-${Math.floor(Math.random() * 4) + 1}`;
+  }, */
+
+  layoutColumnClass() {
+    return '';
+  },
+
+  layoutRowClass(i) {
+    return '';
   },
 
   otherClasses(i) {
---
+++ tests/acceptance/home-page-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | home page', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function() {
+    this.server.createList('panel', 3);
+  });
+
+  test('visiting home page', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-panel]').exists({ count: 3 });
+    assert.dom('[data-test-panel-base-image]').exists({ count: 3 });
+  });
+});
