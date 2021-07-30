8bf9cf8b28bb3ed0b4be259faf2f7bb393110d75
jj.jpg <jayjayjpg@gmail.com>
2021-07-31 01:12:06 +0200

    upd(tests): add a11y testing tools

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,8 +4,14 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { setupGlobalA11yHooks } from 'ember-a11y-testing/test-support';
 
 setApplication(Application.create(config.APP));
+
+setupGlobalA11yHooks(
+  () => true,
+  (_contextSelector, axeOptions) => a11yAudit('#ember-testing-container > *', axeOptions),
+)
 
 setup(QUnit.assert);
 
